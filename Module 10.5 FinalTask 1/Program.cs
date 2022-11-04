b4b906ecde3290a3d1cdcbdc8a5effb7f164b196
Nikula Daniil <daniil.nikula@gmail.com>
2022-11-04 15:18:44 +0200

    Добавьте файлы проекта.

---
+++ Module 10.5 FinalTask 1/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Module_10._5_FinalTask_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CalculateSum calculateSum = new CalculateSum();
+            int a, b;
+            try
+            {
+                Console.WriteLine("Enter first number:");
+                a = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter second number:");
+                b = Convert.ToInt32(Console.ReadLine());
+
+                var result = ((ISum)calculateSum).Sum(a, b);
+                Console.WriteLine("Result:" + result);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+
+    public interface ISum
+    {
+        public int Sum(int one, int two);
+    }
+
+    public class CalculateSum : ISum
+    {
+        int ISum.Sum(int one, int two)
+        {
+            return one + two;
+        }
+    }
+}
